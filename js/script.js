6b62d6bae692e18228c9c2d407e044c3ab4ca59d
dominic-davies <domdaviesit@gmail.com>
2018-12-16 17:09:22 +0000

    initiated isotope plugin, sorted out images folder

--- js/script.js
+++ js/script.js
@@ -5,4 +5,13 @@ $(document).ready(function() {
     pagination: false
   });
   $('[data-fancybox').fancybox();
+
+  $('.items').isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false
+    }
+  });
 });
