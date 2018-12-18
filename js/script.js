d5fc6d96b6f04f82e888c22e2aeb75ab45d5b402
dominic-davies <domdaviesit@gmail.com>
2018-12-18 23:17:01 +0000

    tidy up css file, style showcase text section

--- js/script.js
+++ js/script.js
@@ -10,10 +10,6 @@ $(window).on('load', function() {
 });
 
 $(document).ready(function() {
-  // $('#slides').superslides({
-  //   animation: 'fade',
-  //   play: 5000,
-  //   pagination: false
-  // });
+
   $('[data-fancybox').fancybox();
 });
