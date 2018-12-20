8724e76e4df60547aab494ea05d1307e4f2bb4ee
dominic-davies <domdaviesit@gmail.com>
2018-12-20 19:27:21 +0000

    sticky navbar

--- js/script.js
+++ js/script.js
@@ -30,4 +30,22 @@ $(document).ready(function() {
     showCursor: false
   });
   $('[data-fancybox').fancybox();
+
+  // Fixed Navigation
+  const nav = $('#navigation');
+  const navTop = nav.offset().top;
+
+  $(window).on('scroll', stickyNavigation);
+
+  function stickyNavigation() {
+    var body = $('body');
+
+    if ($(window).scrollTop() >= navTop) {
+      body.css('padding-top', nav.outerHeight() + 'px');
+      body.addClass('fixedNav');
+    } else {
+      body.css('padding-top', 0);
+      body.removeClass('fixedNav');
+    }
+  }
 });
